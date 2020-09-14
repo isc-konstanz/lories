e4a7d8e65a98f41b96de0323d4c2758f178ad0a2
Adrian Minde <Adrian_Minde@live.de>
2020-09-14 18:36:14 +0200

    Update module configs and parameter setup

--- th_e_core/pvsystem.py
+++ th_e_core/pvsystem.py
@@ -10,51 +10,32 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import os
 import pvlib as pv
 
-from configparser import ConfigParser
 from th_e_core.system import Component
 
 
 class PVSystem(Component, pv.pvsystem.PVSystem):
 
     def __init__(self, configs, context, **kwargs):
-        super().__init__(configs, context,
+        super().__init__(configs, context, 
                          name = configs.get('General', 'id'), 
-                         albedo = configs.getfloat('General', 'albedo'), 
-                         surface_tilt = configs.getfloat('Geometry', 'tilt'), 
-                         surface_azimuth = configs.getfloat('Geometry', 'azimuth'), 
-                         modules_per_string = configs.getint('Modules', 'count'), 
-                         strings_per_inverter = configs.getint('Inverter', 'strings'), 
                          **self._init_parameters(configs, context), 
                          **kwargs)
 
-    def _init_parameters(self, configs, *_):
-        with open(os.path.join(configs['General']['config_dir'], configs['General']['id']+'.d', 'module.cfg')) as f:
-            module_file = '[Module]\n' + f.read()
+    def _configure(self, configs, **kwargs):
+        super()._configure(configs, **kwargs)
         
-        module_configs = ConfigParser()
-        module_configs.optionxform = str
-        module_configs.read_string(module_file)
-        module = {}
-        for key, value in module_configs.items('Module'):
-            try:
-                module[key] = float(value)
-                
-            except ValueError:
-                module[key] = value
+        self.surface_tilt = configs.getfloat('Mounting', 'tilt', fallback=0)
+        self.surface_azimuth = configs.getfloat('Mounting', 'azimuth', fallback=180)
         
-        inverter = {}
-        if configs.has_section('Inverter') and 'pdc0' in module:
-            total = configs.getfloat('Inverter', 'strings') \
-                    *configs.getfloat('Modules', 'count') \
-                    *configs.getfloat('General', 'count')
-            
-            inverter['pdc0'] = float(module['pdc0'])*total
+        self.surface_type = configs.getint('General', 'ground_type', fallback=None)
+        if self.surface_type is not None:
+            from pvlib import irradiance
+            self.albedo = irradiance.SURFACE_ALBEDOS.get(self.surface_type, 0.25)
         
-        return { 'module_parameters': module,
-                 'inverter_parameters': inverter }
+        self.strings_per_inverter = configs.getint('Inverter', 'strings', fallback=1)
+        self.modules_per_string = configs.getint('Module', 'count', fallback=1)
 
     @property
     def type(self):
