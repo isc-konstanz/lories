2a404fe9cc9ced16c93a08cad69cb02e5619cb27
Jacob MacWilliams <jakemacwilliams@gmail.com>
2022-03-25 15:18:13 +0100

    Make the Evaluations class the central object of the evaluation module.
    The Evaluations object is now the central object of the evaluations.py module, which entails two main changes. The first of these changes is to make Evaluations the child of Configurable, opposed to Evaluation. This change is mainly aesthetic, and serves the purpose of highlighting that filling out the fields of the configuration file corresponds to the configuration of the Evaluations object (containing a number of Evaluation instances, corresponding to each section of the config file). The second change is for the load_results method to be moved from the Evaluation class to the Evaluations class. Now, each Evaluation instance belonging to an instance of the class Evaluations fetches its data from Evaluations._data, opposed to loading the data itself from the database indicated in the configuration file.

--- th_e_core/evaluation.py
+++ th_e_core/evaluation.py
@@ -23,9 +23,23 @@ logger = logging.getLogger(__name__)
 INVALID_CHARS = "'!@#$%^&?*;:,./\|`´+~=- "
 
 
-class Evaluations(MutableSequence):
+class Evaluations(MutableSequence, Configurable):
 
-    def __init__(self, *evaluations: Evaluation) -> None:
+    def __init__(self, configs: Configurations, *evaluations, **kwargs) -> None:
+
+        Configurable.__init__(self, configs, **kwargs)
+
+        # set in _from_configs
+        self.names = None
+
+        # set in _activate
+        self._database = None
+        self._activate(configs, **kwargs)
+        self.load_results()
+
+        # ToDo: check each name is present
+        # if self.name not in configs.sections():
+        #     raise ValueError('The requested evaluation {} is not present in the configs.'.format(self.name))
 
         self._evaluations = list()
         self._evaluations.extend(evaluations)
@@ -45,54 +59,170 @@ class Evaluations(MutableSequence):
     def __setitem__(self, index: int, system: Evaluation) -> None:
         self._evaluations[index] = system
 
+    def _configure(self, configs: Configurations, **kwargs) -> None:
+        # super()._configure(configs, **kwargs)
+        pass
+
     def insert(self, index: int, system) -> None:
         self._evaluations.insert(index, system)
 
-    def run(self, *args, **kwargs) -> None:
+    def _activate(self, configs: Configurations, **kwargs) -> None:
+
+        if configs.has_section('Database') and \
+                configs.get('Database', 'enabled', fallback='True').lower() == 'true' and \
+                configs.get('Database', 'enable', fallback='True').lower() == 'true':
+
+            if 'dir' in configs['Database']:
+                database_dir = configs['Database']['dir']
+                if not os.path.isabs(database_dir):
+                    configs['Database']['dir'] = os.path.join(configs['General']['data_dir'], database_dir)
+            else:
+                configs['Database']['dir'] = configs['General']['data_dir']
+
+            self._database = Database.open(configs, **kwargs)
+
+        else:
+            return
+
+    @classmethod
+    def read(cls, data_dir: str = 'data', config_scan: bool = False, **kwargs) -> Evaluations:
+
+        if not isinstance(config_scan, bool):
+            config_scan = str(config_scan).lower() == 'true'
+        kwargs['config_scan'] = config_scan
+
+        if config_scan:
+            for system_dir in os.scandir(data_dir):
+                if os.path.isdir(system_dir.path):
+                    configs = cls._read(data_dir=system_dir.path, **kwargs)
+                    evaluations = Evaluations(configs, *cls._from_configs(configs, **kwargs))
+        else:
+            configs = cls._read(data_dir=data_dir, **kwargs)
+            evaluations = Evaluations(configs, *cls._from_configs(configs))
+
+        return evaluations
+
+    @classmethod
+    def _read(cls,
+              root_dir:    str = '.',
+              lib_dir:     str = 'lib',
+              tmp_dir:     str = 'tmp',
+              data_dir:    str = 'data',
+              config_dir:  str = 'conf',
+              config_name: str = None,
+              **kwargs) -> Configurations:
+
+        if config_name is None:
+            config_name = cls.__name__.lower() + '.cfg'
+
+        configs = cls._read_configs(root_dir, lib_dir, tmp_dir, data_dir, config_dir, config_name, **kwargs)
+
+        package = kwargs.get('package') if 'package' in kwargs else '.'.join(cls.__module__.split('.')[:-1])
+        module = kwargs.get('module') if 'module' in kwargs else cls.__module__.split('.')[-1]
+
+        if 'Import' not in configs.sections():
+            configs.add_section('Import')
+
+        if configs.has_option('General', 'type') and not configs.get('General', 'type').lower() == 'default':
+            configs.set('Import', 'class', configs.get('General', 'type'))
+        elif not configs.has_option('Import', 'class'):
+            configs.set('Import', 'class', cls.__name__)
+        if not configs.has_option('Import', 'module'):
+            configs.set('Import', 'module', module)
+        if not configs.has_option('Import', 'package'):
+            configs.set('Import', 'package', package)
+
+        return configs
+
+    @staticmethod
+    def _from_configs(configs: Configurations, *args, **kwargs) -> List[Evaluation]:
+
+        evaluations = list()
+
+        try:
+            obj = __import__(configs['Import']['package']+'.'+configs['Import']['module'],
+                             fromlist=[configs['Import']['class']])
+
+        except ModuleNotFoundError as error:
+            logger.debug(error)
+
+            configs.set('Import', 'package', 'th_e_core')
+            obj = __import__('th_e_core.'+configs['Import']['module'], fromlist=[configs['Import']['class']])
+
+        if configs.has_option('General', 'names'):
+
+            names = configs['General']['names'].split(', ')
+            for name in names:
+                eval_configs = dict(configs[name].items())
+                evaluations.append(getattr(obj, configs['Import']['class'])(name,  **eval_configs))
+
+        else:
+            raise ValueError("Invalid configuration, missing subsection names in section 'General'.")
+
+        return evaluations
+
+    def load_results(self):
+
+        data_path = os.path.join(self._database.dir, 'results.h5')
+
+        if not os.path.isfile(data_path):
+            raise FileExistsError("The requisite file {} does not exist.".format(data_path))
+
+        datastore = pd.HDFStore(data_path)
+        results = pd.DataFrame()
+
+        for date_path in datastore:
+
+            if date_path.endswith('outputs'):
+
+                result = datastore.get(date_path)
+                results = pd.concat([results, result], axis=0)
+
+        results = results.sort_index()
+        results['day_hour'] = [t.hour for t in results.index]
+        results['weekday'] = [t.weekday for t in results.index]
+        results['month'] = [t.month for t in results.index]
+
+        # ToDo: Move check to appropriate location in Evaluation class
+        #if not self._cols.issubset(set(results.columns)):
+        #    _na = self._cols.difference(set(results.columns))
+        #    raise ValueError("Unable to load data corresponding to the path {}, as the"
+        #                     " columns {} required for the evaluation {} were not present"
+        #                     ".".format(data_path, _na, self.name))
+
+        results.index = [i for i in range(len(results))]
+
+        self._database.close()
+        self._database = results
+        datastore.close()
+
+    def run(self) -> None:
+        from copy import deepcopy
+
         for evaluation in self:
-            evaluation.run(*args, **kwargs)
+            evaluation._data = deepcopy(self._database)
+            evaluation.run()
 
 
-class Evaluation(Configurable):
+class Evaluation:
 
-    def __init__(self, name, configs: Configurations, **kwargs) -> None:
+    def __init__(self, name, targets, metrics, groups, group_bins=None, conditions=None, summaries=None, data=None, boxplots=None, **kwargs) -> None:
 
         self.name = name
-
-        if self.name not in configs.sections():
-            raise ValueError('The requested evaluation {} is not present in the configs.'.format(self.name))
-
-        super().__init__(configs, **kwargs)
-
-        req = {'targets', 'metrics', 'groups'}
-        #opt = {'conditions', 'summaries', 'boxplots'}
-
-        if not req.issubset(set(configs[self.name].keys())):
-            raise ValueError('Invalid configuration, missing attribute in configs.'
-                             ' Check that the following are present {}'.format(req))
+        self._data = data
+        self.data = None
 
         # necessary
-        self.targets = configs[self.name]['targets']
-        self.metrics = configs[self.name]['metrics']
-        self.groups = configs[self.name]['groups']
+        self.targets = targets
+        self.metrics = metrics
+        self.groups = groups
 
         # optional
-        if configs.has_option(self.name, 'group_bins'):
-            self.group_bins = configs[self.name]['group_bins']
+        self.group_bins = group_bins
+        self.boxplots = boxplots
+        self.conditions = conditions
+        self.summaries = summaries
 
-        if configs.has_option(self.name, 'boxplots'):
-            self.boxplots = configs[self.name]['boxplots']
-
-        if configs.has_option(self.name, 'conditions'):
-            self.conditions = configs[self.name]['conditions']
-
-        if configs.has_option(self.name, 'summaries'):
-            self.summaries = configs[self.name]['summaries']
-
-        self._activate(configs, **kwargs)
-
-        self._data = None
-        self.data = None
 
         # Outputs
         self.evaluation = pd.DataFrame()
@@ -101,7 +231,7 @@ class Evaluation(Configurable):
 
         # private
         _cols = list()
-        if self.configs.has_option(self.name, 'conditions'):
+        if conditions:
             for condition in self.conditions:
                 _cols.append(condition[0])
 
@@ -184,16 +314,20 @@ class Evaluation(Configurable):
     @group_bins.setter
     def group_bins(self, value):
 
-        values = value.split(', ')
-        new_values = list()
+        if not value:
+            self._group_bins = None
 
-        while values:
+        else:
+            values = value.split(', ')
+            new_values = list()
 
-            t = values.pop().lower()
-            t = int(t)
-            new_values.append(t)
+            while values:
 
-        self._group_bins = new_values
+                t = values.pop().lower()
+                t = int(t)
+                new_values.append(t)
+
+            self._group_bins = new_values
 
     @property
     def boxplots(self):
@@ -202,20 +336,25 @@ class Evaluation(Configurable):
     @boxplots.setter
     def boxplots(self, value):
 
-        values = value.split(', ')
-        new_values = list()
+        if not value:
+            self._boxplots = None
 
-        while values:
+        else:
 
-            t = values.pop().lower()
-            truth = ['1', 'true', 'yes']
+            values = value.split(', ')
+            new_values = list()
 
-            if t in truth:
-                new_values.append(True)
-            else:
-                new_values.append(False)
+            while values:
 
-        self._boxplots = new_values
+                t = values.pop().lower()
+                truth = ['1', 'true', 'yes']
+
+                if t in truth:
+                    new_values.append(True)
+                else:
+                    new_values.append(False)
+
+            self._boxplots = new_values
 
     @property
     def conditions(self):
@@ -224,23 +363,27 @@ class Evaluation(Configurable):
     @conditions.setter
     def conditions(self, value):
 
-        values = value.split(', ')
-        new_values = list()
+        if not value:
+            self.conditions = None
 
-        while values:
+        else:
+            values = value.split(', ')
+            new_values = list()
 
-            t = values.pop().lower()
+            while values:
 
-            # Ensure proper formatting of target string (no special signs or spaces)
-            # ToDo: Ensure that name is still a valid variable in the future
-            if re.match('[^a-zA-Z0-9-_><=.,]+', t):
-                raise ValueError('The condition {} in the evaluation {} contains unsupported characters'.format(t, self.name))
+                t = values.pop().lower()
 
-            t = t.split(' ')
-            t[2] = int(t[2])
-            new_values.append(t)
+                # Ensure proper formatting of target string (no special signs or spaces)
+                # ToDo: Ensure that name is still a valid variable in the future
+                if re.match('[^a-zA-Z0-9-_><=.,]+', t):
+                    raise ValueError('The condition {} in the evaluation {} contains unsupported characters'.format(t, self.name))
 
-        self._conditions = new_values
+                t = t.split(' ')
+                t[2] = int(t[2])
+                new_values.append(t)
+
+            self._conditions = new_values
 
     @property
     def summaries(self):
@@ -249,154 +392,28 @@ class Evaluation(Configurable):
     @summaries.setter
     def summaries(self, value):
 
-        valid_sum = ['horizon_weighted', 'mean', 'high_load_bias',
-                     'err_per_load', 'optimist', 'fullest_bin']
+        if not value:
+            self._summaries = None
 
-        values = value.split(', ')
-        new_values = list()
-
-        while values:
-
-            t = values.pop().lower()
-
-            if t not in valid_sum:
-                raise ValueError('The summary {} passed for the evaluation {} is not valid. Please ensure'
-                                 ' that all chosen summaries are present in the following list:'
-                                 ' {}'.format(t, self.name, valid_sum))
-
-            new_values.append(t)
-
-        self._summaries = new_values
-
-    def _configure(self, configs: Configurations, **kwargs) -> None:
-        # super()._configure(configs, **kwargs)
-        pass
-
-    def _activate(self, configs: Configurations, **kwargs) -> None:
-        if configs.has_section('Database') and \
-                configs.get('Database', 'enabled', fallback='True').lower() == 'true' and \
-                configs.get('Database', 'enable', fallback='True').lower() == 'true':
-
-            if 'dir' in configs['Database']:
-                database_dir = configs['Database']['dir']
-                if not os.path.isabs(database_dir):
-                    configs['Database']['dir'] = os.path.join(configs['General']['data_dir'], database_dir)
-            else:
-                configs['Database']['dir'] = configs['General']['data_dir']
-
-            self._database = Database.open(configs, **kwargs)
         else:
-            self._database = None
+            valid_sum = ['horizon_weighted', 'mean', 'high_load_bias',
+                         'err_per_load', 'optimist', 'fullest_bin']
 
-    @classmethod
-    def read(cls, data_dir: str = 'data', config_scan: bool = False, **kwargs) -> Evaluations:
+            values = value.split(', ')
+            new_values = list()
 
-        if not isinstance(config_scan, bool):
-            config_scan = str(config_scan).lower() == 'true'
-        kwargs['config_scan'] = config_scan
+            while values:
 
-        if config_scan:
-            for system_dir in os.scandir(data_dir):
-                if os.path.isdir(system_dir.path):
-                    configs = cls._read(data_dir=system_dir.path, **kwargs)
-                    evaluations = Evaluations(*cls._from_configs(configs, **kwargs))
-        else:
-            configs = cls._read(data_dir=data_dir, **kwargs)
-            evaluations = Evaluations(*cls._from_configs(configs))
+                t = values.pop().lower()
 
-        return evaluations
+                if t not in valid_sum:
+                    raise ValueError('The summary {} passed for the evaluation {} is not valid. Please ensure'
+                                     ' that all chosen summaries are present in the following list:'
+                                     ' {}'.format(t, self.name, valid_sum))
 
-    @classmethod
-    def _read(cls,
-              root_dir:    str = '.',
-              lib_dir:     str = 'lib',
-              tmp_dir:     str = 'tmp',
-              data_dir:    str = 'data',
-              config_dir:  str = 'conf',
-              config_name: str = None,
-              **kwargs) -> Configurations:
+                new_values.append(t)
 
-        if config_name is None:
-            config_name = cls.__name__.lower() + '.cfg'
-
-        configs = cls._read_configs(root_dir, lib_dir, tmp_dir, data_dir, config_dir, config_name, **kwargs)
-
-        package = kwargs.get('package') if 'package' in kwargs else '.'.join(cls.__module__.split('.')[:-1])
-        module = kwargs.get('module') if 'module' in kwargs else cls.__module__.split('.')[-1]
-
-        if 'Import' not in configs.sections():
-            configs.add_section('Import')
-
-        if configs.has_option('General', 'type') and not configs.get('General', 'type').lower() == 'default':
-            configs.set('Import', 'class', configs.get('General', 'type'))
-        elif not configs.has_option('Import', 'class'):
-            configs.set('Import', 'class', cls.__name__)
-        if not configs.has_option('Import', 'module'):
-            configs.set('Import', 'module', module)
-        if not configs.has_option('Import', 'package'):
-            configs.set('Import', 'package', package)
-
-        return configs
-
-    @staticmethod
-    def _from_configs(configs: Configurations, *args, **kwargs) -> List[Evaluation]:
-
-        evaluations = list()
-
-        try:
-            obj = __import__(configs['Import']['package']+'.'+configs['Import']['module'],
-                             fromlist=[configs['Import']['class']])
-
-        except ModuleNotFoundError as error:
-            logger.debug(error)
-
-            configs.set('Import', 'package', 'th_e_core')
-            obj = __import__('th_e_core.'+configs['Import']['module'], fromlist=[configs['Import']['class']])
-
-        if configs.has_option('General', 'names'):
-
-            names = configs['General']['names'].split(', ')
-            for name in names:
-
-                evaluations.append(getattr(obj, configs['Import']['class'])(name, configs, *args, **kwargs))
-        else:
-            raise ValueError("Invalid configuration, missing subsection names in section 'General'.")
-
-        return evaluations
-
-    def load_results(self):
-
-        data_path = os.path.join(self._database.dir, 'results.h5')
-
-        if not os.path.isfile(data_path):
-            raise FileExistsError("The requisite file {} does not exist.".format(data_path))
-
-        datastore = pd.HDFStore(data_path)
-        results = pd.DataFrame()
-
-        for date_path in datastore:
-
-            if date_path.endswith('outputs'):
-
-                result = datastore.get(date_path)
-                results = pd.concat([results, result], axis=0)
-
-        results = results.sort_index()
-        results['day_hour'] = [t.hour for t in results.index]
-        results['weekday'] = [t.weekday for t in results.index]
-        results['month'] = [t.month for t in results.index]
-
-        if not self._cols.issubset(set(results.columns)):
-            _na = self._cols.difference(set(results.columns))
-            raise ValueError("Unable to load data corresponding to the path {}, as the"
-                             " columns {} required for the evaluation {} were not present"
-                             ".".format(data_path, _na, self.name))
-
-        results.index = [i for i in range(len(results))]
-        self._data = results
-
-        self._database.close()
-        datastore.close()
+            self._summaries = new_values
 
     def _extract_labels(self):
 
@@ -493,10 +510,10 @@ class Evaluation(Configurable):
 
         self.data = self._data[self._cols]
 
-        if self._configs.has_option(self.name, 'conditions'):
+        if self.conditions:
             self._select_data()
 
-        if self._configs.has_option(self.name, 'group_bins'):
+        if self.group_bins:
             self._extract_labels()
 
     def perform_metric(self, data, target, metric):
@@ -620,7 +637,6 @@ class Evaluation(Configurable):
     def run(self):
         from copy import deepcopy
 
-        self.load_results()
         self.prepare_data()
 
         cols = [col for col in self.data.columns if not col.endswith('_err')]
