7331abbe08b93518be105845284043d59399a7d1
Adrian Minde <adrian.minde@mail.de>
2025-01-15 08:31:26 +0100

    Minor newline fix

--- lori/util.py
+++ lori/util.py
@@ -56,6 +56,7 @@ def get_includes(type: Type) -> List[str]:
                 includes.append(include)
     return includes
 
+
 # noinspection PyShadowingBuiltins, PyShadowingNames
 def get_variables(
     object: Any,
