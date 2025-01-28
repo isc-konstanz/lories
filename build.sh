2f73df97d8a34c317ffc9a7f8ab6d25e462909fa
Adrian Minde <adrian.minde@mail.de>
2025-01-28 11:08:59 +0100

    Fix dpkg dir creation

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ while [ -h "$dir" ] ; do
 		dir=`dirname "$dir"`"/$link"
 	fi
 done
-cd "`dirname \"$dir\"`/.." >/dev/null
+cd "`dirname \"$dir\"`" >/dev/null
 lori_dir="`pwd -P`"
 build_dir="$lori_dir/build/dpkg"
 
@@ -28,22 +28,29 @@ rm -rf $build_dir
 mkdir -p $build_dir
 
 cd $build_dir
-cp -r $lori_dir/lib/debian $build_dir/
-chmod 755 $build_dir/debian/pre* 2>/dev/null
-chmod 755 $build_dir/debian/post* 2>/dev/null
-chmod 755 $build_dir/debian/rules
+cp -r "$lori_dir/lib/debian" "$build_dir/"
+chmod 755 "$build_dir/debian/pre*" 2>/dev/null
+chmod 755 "$build_dir/debian/post*" 2>/dev/null
+chmod 755 "$build_dir/debian/rules"
 
-version=$(<$build_dir/debian/version)
-sed -i "s/<version>/$version/g" $build_dir/debian/changelog
-sed -i "s/<version>/$version/g" $build_dir/debian/control
-sed -i "s/<version>/$version/g" $build_dir/debian/postinst
+echo "$build_dir/debian/version"
+if [ -f "$build_dir/debian/version" ]; then
+    echo "Yep"
+fi
+version=$(cat "$build_dir/debian/version")
+rm "$build_dir/debian/version"
 
-cp -r $lori_dir/lib/systemd/* $build_dir/debian/
-cp -r $lori_dir/lib/tmpfiles.d $build_dir/
+sed -i "s/<version>/$version/g" "$build_dir/debian/changelog"
+sed -i "s/<version>/$version/g" "$build_dir/debian/control"
+sed -i "s/<version>/$version/g" "$build_dir/debian/postinst"
 
-mkdir -p $build_dir/etc
-cp -r $lori_dir/logging.default.conf $build_dir/etc/logging.conf
-cp -r $lori_dir/settings.default.conf $build_dir/etc/settings.conf
+cp "$lori_dir/lib/systemd/lori.service" "$build_dir/debian/"
+
+cp -r "$lori_dir/lib/tmpfiles.d" "$build_dir/"
+
+mkdir -p "$build_dir/etc"
+cp -r "$lori_dir/conf/logging.default.conf" "$build_dir/etc/logging.conf"
+cp -r "$lori_dir/conf/settings.default.conf" "$build_dir/etc/settings.conf"
 
 dpkg-buildpackage -us -uc
 exit 0
