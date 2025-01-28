fe6a129fdfcfacc6f6bdc706f17ef756fcf09073
Adrian Minde <adrian.minde@mail.de>
2025-01-28 10:42:30 +0100

    First debian package commit

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#Scriptname: build.sh
+#Description: script to build lori debian packages with dpkg
+
+if [ $(id -u) != 0 ]; then
+    echo "DPKG build process should be performed with root privileges." 1>&2
+    exit 1
+fi
+
+# Attempt to set lori_dir
+# Resolve links: $0 may be a link
+dir="$0"
+# Need this for relative symlinks.
+while [ -h "$dir" ] ; do
+	ls=`ls -ld "$dir"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '/.*' > /dev/null; then
+		dir="$link"
+	else
+		dir=`dirname "$dir"`"/$link"
+	fi
+done
+cd "`dirname \"$dir\"`/.." >/dev/null
+lori_dir="`pwd -P`"
+build_dir="$lori_dir/build/dpkg"
+
+rm -rf $build_dir
+mkdir -p $build_dir
+
+cd $build_dir
+cp -r $lori_dir/lib/debian $build_dir/
+chmod 755 $build_dir/debian/pre* 2>/dev/null
+chmod 755 $build_dir/debian/post* 2>/dev/null
+chmod 755 $build_dir/debian/rules
+
+version=$(<$build_dir/debian/version)
+sed -i "s/<version>/$version/g" $build_dir/debian/changelog
+sed -i "s/<version>/$version/g" $build_dir/debian/control
+sed -i "s/<version>/$version/g" $build_dir/debian/postinst
+
+cp -r $lori_dir/lib/systemd/* $build_dir/debian/
+cp -r $lori_dir/lib/tmpfiles.d $build_dir/
+
+mkdir -p $build_dir/etc
+cp -r $lori_dir/logging.default.conf $build_dir/etc/logging.conf
+cp -r $lori_dir/settings.default.conf $build_dir/etc/settings.conf
+
+dpkg-buildpackage -us -uc
+exit 0
