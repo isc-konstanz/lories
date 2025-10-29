f6d2788ec9c233e7732749bb8fe16b7376b732c3
Jonas <jonasbechler@gmail.com>
2025-10-29 07:47:45 +0100

    Fix JsonConverter handling colons in path

--- lories/data/converters/json.py
+++ lories/data/converters/json.py
@@ -9,11 +9,12 @@ lories.data.converters.json
 from __future__ import annotations
 
 import json
-import jsonpath_ng as jsonpath
 from typing import Any, AnyStr, Optional, Sequence
 
+import jsonpath_ng as jsonpath
+
 from lories.data.converters.context import register_converter_type
-from lories.data.converters.converter import StringConverter, ConversionError
+from lories.data.converters.converter import ConversionError, StringConverter
 
 
 # noinspection PyMethodMayBeStatic
@@ -23,7 +24,7 @@ class JsonConverter(StringConverter):
         if isinstance(value, str):
             try:
                 json_data = json.loads(value)
-                json_path = jsonpath.parse(path)
+                json_path = jsonpath.parse(self._fix_path(path))
                 json_match = json_path.find(json_data)
 
                 if len(json_match) == 1:
@@ -34,3 +35,22 @@ class JsonConverter(StringConverter):
             except json.JSONDecodeError as e:
                 raise ConversionError(e) from e
         return None
+    
+    def _fix_path(self, path: str) -> str:
+        """
+        Fix JSONPath to handle keys with colon (:)
+        Example:
+            sensor.temperature:avg.value  -> sensor.["temperature:avg"].value
+        """
+        if ":" not in path:
+            return path
+        
+        # parts = path.split(".")
+        # fixed_parts = []
+        # for part in parts:
+        #     if ":" in part:
+        #         fixed_parts.append(f'["{part}"]')
+        #     else:
+        #         fixed_parts.append(part)
+        # return ".".join(fixed_parts)
+        return ".".join([f'["{part}"]' if ":" in part else part for part in path.split(".")])
