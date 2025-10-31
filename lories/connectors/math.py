c10d9db7d9eb9926c67a5458c9ba72bfb3fbd436
Jonas Bechler <jonasbechler@gmail.com>
2025-10-31 08:29:56 +0100

    Refactor MathConnector configuration and evaluation logic

--- lories/connectors/math.py
+++ lories/connectors/math.py
@@ -8,7 +8,7 @@ lories.connectors.math
 
 from __future__ import annotations
 
-from typing import Dict, Optional, Any, Callable
+from typing import Any, Dict, Optional
 
 import sympy
 
@@ -26,57 +26,84 @@ class MathConnector(Connector):
 
     _connected: bool
 
-    _channels: Dict[str, Channel]
     _expr: sympy.Expr
-
-    _values: Dict[str, Any]
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    _symbols: list[InputSymbol]
 
     def configure(self, configs: Configurations) -> None:
         super().configure(configs)
         self._connected = False
+        self._symbols = []
 
-        self._channels = configs.get("channels", {})
-        math = configs.get("math", "")
-        if not self._channels or math == "":
-            raise ConfigurationError("MathConnector requires 'channels' and 'math' configurations.")
-
+        math = configs.get("math")
         try:
             expr = sympy.sympify(math)
             expr = sympy.simplify(expr)
             self._expr = expr
         except Exception as e:
-            raise ConfigurationError(f"Error parsing math expression: {e}")
+            raise ConfigurationError(f"Error parsing math expression: {math}: {e}")
+        
+        mapping = configs.get("mapping", {})
+        for symbol in expr.free_symbols:
+            input_symbol = InputSymbol(symbol)
+            if symbol.name in mapping:
+                input_symbol.update_id(mapping[symbol.name])
+                mapping.pop(symbol.name)
+            self._symbols.append(input_symbol)
+            
+        for unmapped in mapping.items():
+            raise ConfigurationError(f"Mapping for unknown symbol '{unmapped}'")
 
-        symbols = sorted(expr.free_symbols, key=lambda s: s.name)
-        symbol_names = [s.name for s in symbols]
-        channel_aliases = list(self._channels.keys())
-        if sorted(symbol_names) != sorted(channel_aliases):
-            raise ConfigurationError(
-                f"Symbols in math expression {symbol_names} do not match channel aliases {channel_aliases}."
-            )
 
     def is_connected(self) -> bool:
         return self._connected
 
     def connect(self, resources: Resources) -> None:
         # Todo: is filtering necessary?
-        channels = resources.filter(lambda r: isinstance(r, Channel))
-        channel_ids = [ch.id for ch in channels]
-        conf_channel_ids = [list(self._channels.values())]
+        #channels = resources.filter(lambda r: isinstance(r, Channel))
+        # resources is only the output resources (channels) assigned to this connector, which receive the computed values
 
-        if sorted(channel_ids) != sorted(conf_channel_ids):
-            raise ConfigurationError(
-                f"Resources provided {channel_ids} do not match configured channels {conf_channel_ids}."
-            )
+        
+        for symbol in self._symbols:
+            channel = self._find_channel(symbol.channel_id)
+            if channel is None:
+                raise ConfigurationError(
+                    f"Channel '{symbol.channel_id}' for symbol '{symbol.name}' not found."
+                )
+            symbol.update_channel(channel)
+            symbol.update_value()
+            self.context.context.data.register(symbol, channel, how="any", unique=False)
+            # Todo: call _evaluate when symbol channel is updated
 
+        
+        self._evaluate()
         self._connected = True
+        
+    def _find_channel(self, channel_id: str) -> Optional[Channel]:
+        if "." in channel_id:
+            # Todo: find globally by id
+            raise NotImplementedError("Finding channels by global id is not implemented yet.")
+        else:
+            # Todo: not tested
+            component = self.context.context
+            return component.data.get(channel_id)
+        
+    def _evaluate(self) -> None:
+        timestamp = pd.Timestamp.now(tz=tz.UTC)
+        local_dict = {symbol.name: symbol.value for symbol in self._symbols}
+        if any(value is None for value in local_dict.values()):
+            return
+        
+        result = self._expr.evalf(subs=local_dict)
+        for channel in self.resources.filter(lambda r: isinstance(r, Channel)):
+            channel.set(timestamp, result)
+        
 
     def disconnect(self) -> None:
         self._connected = False
+        for symbol in self._symbols:
+            self.context.context.data.unregister(symbol)
+            del symbol.channel
+            
 
     def read(self, resources: Resources) -> pd.DataFrame:
         pass
@@ -84,8 +111,31 @@ class MathConnector(Connector):
     def write(self, data: pd.DataFrame) -> None:
         pass
 
-    def _update_values(self, data: pd.DataFrame) -> None:
-        pass
 
-    def _eval(self):
-        pass
+
+
+class InputSymbol:
+    name: str
+    channel_id: str
+    channel: Channel
+    value: Any
+
+    def __init__(self, symbol: sympy.Symbol) -> None:
+        self.name = symbol.name
+        self.channel_id = self.name
+        self.channel = None
+        self.value = None
+        
+    def __call__(self, data: pd.DataFrame) -> None:
+        self.value = data.at[data.index[-1], self.channel.id]
+        
+    def update_id(self, channel_id: str) -> None:
+        self.channel_id = channel_id
+        
+    def update_channel(self, channel: Channel) -> None:
+        self.channel = channel
+        
+    def update_value(self) -> None:
+        if self.channel is not None:
+            self.value = self.channel.value
+    
