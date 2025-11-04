825ff7d4013f4e9c671ed76f417b702f673d1543
Jonas Bechler <jonasbechler@gmail.com>
2025-11-04 11:12:59 +0100

    Refactor OpcUaConnector: logging, error handling, PEP 8

--- lories/connectors/opcua.py
+++ lories/connectors/opcua.py
@@ -8,15 +8,16 @@ lories.connectors.opcua
 
 from __future__ import annotations
 
+import logging
+import math
 from typing import Dict, Optional
 
 import opcua
-
 import pandas as pd
 import pytz as tz
+
 from lories._core import ChannelState  # noqa
 from lories.connectors import Connector, register_connector_type
-from lories.core import ConfigurationError
 from lories.data import Channel
 from lories.typing import Configurations, Resources
 
@@ -38,31 +39,34 @@ class OpcUaConnector(Connector):
     def configure(self, configs: Configurations) -> None:
         super().configure(configs)
 
-
         self._host = configs.get("host", default="127.0.0.1")
         self._port = configs.get_int("port", default=4840)
         self._timeout = configs.get_int("timeout", default=60)
-        self._settings = [s.strip() for s in configs.get("settings", default="").split(",")]
-        #self._endpoint = configs.get("endpoint", default="")
-        
+        settings = configs.get("settings", default="").split(",")
+        self._settings = [s.strip() for s in settings]
+
+        # Todo: is endpoint needed / wanted?
+        # self._endpoint = configs.get("endpoint", default="")
+
         self._client = opcua.Client(
             f"opc.tcp://{self._host}:{self._port}",
             timeout=self._timeout
         )
-    
+
         if "username" in configs and "password" in configs:
             self._client.set_user(configs.get("username"))
             self._client.set_password(configs.get("password"))
-            
+
         self._nodes = {}
 
-
+        # Disable all log messages from the 'opcua' logger
+        logging.getLogger("opcua").setLevel(logging.CRITICAL)
 
     def is_connected(self) -> bool:
         return self._client is not None \
             and True
-            # self._client.uaclient._uasocket is not None and \
-            # self._client.uaclient._uasocket.connected
+        # self._client.uaclient._uasocket is not None and \
+        # self._client.uaclient._uasocket.connected
 
     def connect(self, resources: Resources) -> None:
         self._client.connect()
@@ -72,12 +76,14 @@ class OpcUaConnector(Connector):
         for channel in channels:
             try:
                 address = channel.get("address", channel.id)
-                node_name = ";".join([*self._settings.split(","), f"s={address}"])
+                node_name = ";".join([*self._settings, f"s={address}"])
                 node = self._client.get_node(node_name.strip())
                 self._nodes[channel.id] = node
             except Exception as e:
-                self._logger.warning(f"Failed to get OPC UA node for channel '{channel.id}': {e}")
-        
+                self._logger.warning(
+                    f"Failed to get OPC UA node for '{channel.id}': {e}"
+                )
+
     def disconnect(self) -> None:
         if self.is_connected():
             self._client.disconnect()
@@ -87,32 +93,39 @@ class OpcUaConnector(Connector):
     def read(self, resources: Resources) -> pd.DataFrame:
         timestamp = pd.Timestamp.now(tz.UTC).floor(freq="s")
         data = pd.DataFrame(index=[timestamp], columns=resources.ids)
-        
+
         for channel in resources:
             node = self._nodes.get(channel.id)
             if node is None:
-                self._logger.warning(f"Node for channel '{channel.id}' not found")
+                self._logger.warning(f"Node for '{channel.id}' not found")
+
                 data.at[timestamp, channel.id] = ChannelState.NOT_AVAILABLE
                 continue
-            
+
             try:
                 value = node.get_value()
+                if math.isnan(value):
+                    value = ChannelState.NOT_AVAILABLE
                 data.at[timestamp, channel.id] = value
             except Exception as e:
-                self._logger.warning(f"Failed to read value for channel '{channel.id}': {e}")
                 data.at[timestamp, channel.id] = ChannelState.NOT_AVAILABLE
+                self._logger.warning(
+                    f"Failed to read value for '{channel.id}': {e}"
+                )
         return data
 
     def write(self, data: pd.DataFrame) -> None:
         for channel in self.channels:
             node = self._nodes.get(channel.id)
             if node is None:
-                self._logger.warning(f"Node for channel '{channel.id}' not found")
-                continue
-            
-            try:
-                value = data.at[data.index[-1], channel.id]
-                node.set_value(value)
-            except Exception as e:
-                self._logger.warning(f"Failed to write value for channel '{channel.id}': {e}")
-    
+                self._logger.warning(
+                    f"Node for '{channel.id}' not found"
+                )
+            else:
+                try:
+                    value = data.at[data.index[-1], channel.id]
+                    node.set_value(value)
+                except Exception as e:
+                    self._logger.warning(
+                        f"Failed to write value for '{channel.id}': {e}"
+                    )
