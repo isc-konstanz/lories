0aff256afe31d7001012939c9e57a25b96e8c496
jb <jonasbechler@gmail.com>
2026-03-07 01:15:17 +0100

    Add filter implementations to the filters module

--- lories/data/filters/__init__.py
+++ lories/data/filters/__init__.py
@@ -7,3 +7,5 @@ lories.data.filters
 """
 
 from .filter import Filter  # noqa: F401
+from .ir_filters import IirFilter, FirFilter  # noqa: F401
+from .basic_filters import MovingAverageFilter, LowPassFilter  # noqa: F401
---
+++ lories/data/filters/basic_filters.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+lories.data.filters.basic_filters
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+"""
+
+from __future__ import annotations
+
+from lories.data.filters.basic_filters import IirFilter, FirFilter
+
+
+class LowPassFilter(IirFilter):
+    def __init__(self, _type, id = None, key = None, name = None, enabled = True, **configs):
+        decay = configs.get("decay", 0.5)
+        configs["numerator"] = [1 - decay]
+        configs["denominator"] = [1, -decay]
+
+        super().__init__(_type, id, key, name, enabled, **configs)
+
+
+class MovingAverageFilter(FirFilter):
+    def __init__(self, _type, id = None, key = None, name = None, enabled = True, **configs):
+        length = configs.get("length", 1)
+        configs["coefficients"] = [1 / length] * length
+
+        super().__init__(_type, id, key, name, enabled, **configs)
+
+
+
