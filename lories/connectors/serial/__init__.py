729469694d25680faf87dce04d06f77eb71ddf73
Jonas Bechler <jonasbechler@gmail.com>
2026-04-23 13:49:28 +0200

    Add I2C serial connector with BME280 sensor support

--- lories/connectors/serial/__init__.py
+++ lories/connectors/serial/__init__.py
@@ -5,20 +5,3 @@ lories.connectors.serial
 
 
 """
-
-import importlib
-
-CONNECTORS = [
-    "sdi12",
-    "i2c",
-]
-
-for import_connector in CONNECTORS:
-    try:
-        importlib.import_module(f".{import_connector}", "lories.connectors.serial")
-
-    except ModuleNotFoundError:
-        # TODO: Implement meaningful logging here
-        pass
-
-del importlib
